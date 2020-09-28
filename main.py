f796ab940e26d1f6c2e15c1e2b7b6f2462c5923f
jjp6315 <jjp6315@psu.edu>
2020-09-28 03:19:31 +0000

    period

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ def getGradePoint(grade):
 def run():
   grade = str(input("Enter your course 1 letter grade: "))
   credit1 = float(input("Enter your course 1 credit: "))
-  print(f"Grade point for course 1 is: {getGradePoint (grade)}.")
+  print(f"Grade point for course 1 is: {getGradePoint (grade)}")
   GPA1 = {getGradePoint (grade)} * credit1
 
   grade = str(input("Enter your course 2 letter grade: "))
   credit2 = float(input("Enter your course 2 credit: "))
-  print(f"Grade point for course 2 is: {getGradePoint (grade)}.")
+  print(f"Grade point for course 2 is: {getGradePoint (grade)}")
   GPA2 = {getGradePoint (grade)} * credit2
 
   grade = str(input("Enter your course 3 letter grade: "))
   credit3 = float(input("Enter your course 3 credit: "))
-  print(f"Grade point for course 3 is: {getGradePoint (grade)}.")
+  print(f"Grade point for course 3 is: {getGradePoint (grade)}")
   GPA3 = {getGradePoint (grade)} * credit3
 
   GPA = (GPA1 + GPA2 + GPA3) / (credit1 + credit2 + credit3) 
